96b6610c4f31664c5ea9acf239db0348d0c28e6d
Alouhette <evan.lippens@gmail.com>
2025-05-14 15:46:52 +0200

    Created Interface and 2 Solver (Bases) (WIP)

---
+++ src/main/java/fr/cytech/projetgenielogiciel/Solver/AStartSolver.java
@@ -0,0 +1,65 @@
+package fr.cytech.projetgenielogiciel.Solver;
+
+import fr.cytech.projetgenielogiciel.maze.Cell;
+import fr.cytech.projetgenielogiciel.maze.Maze;
+
+public class AStartSolver implements ISolver{
+    protected Maze laby;
+    protected boolean solved;
+
+
+    /** Creator of Solver class
+     * @param lab take a maze that will be solved step by step
+     *
+     */
+    public AStartSolver(Maze lab){
+        try{
+            if(lab == null){
+                throw new IllegalArgumentException("labyrinthe null");
+            }
+            this.laby = lab;
+            this.solved = false;
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public Maze getLaby(){
+        return this.laby;
+    }
+
+    public void setLaby(Maze lab) {
+        if (lab != null) {
+            this.laby = lab;
+        }
+    }
+
+
+    /** Does one step from the cell c
+     * @param c actual position in the maze
+     */
+    public void step(Cell c){
+
+    }
+
+    /** Solve the maze by starting from s to f
+     * @param s start of the maze
+     * @param f end of the maze
+     */
+    public void solve(Cell s,Cell f){
+
+    }
+
+    // ???
+    public boolean isFinished(){
+        return false;
+    }// Return if it is solved or not (litteraly getSolved)
+
+    public float heuristic(Cell c){
+        return 0;
+    }
+
+
+
+}
---
+++ src/main/java/fr/cytech/projetgenielogiciel/Solver/ISolver.java
@@ -0,0 +1,21 @@
+package fr.cytech.projetgenielogiciel.Solver;
+import fr.cytech.projetgenielogiciel.maze.Cell;
+
+interface ISolver {
+
+    //no need for toString & equals
+    /** Does one step from the cell c
+     * @param c actual position in the maze
+     */
+    public void step(Cell c);
+
+    /** Solve the maze by starting from s to f
+     * @param s start of the maze
+     * @param f end of the maze
+     */
+    public void solve(Cell s,Cell f);
+
+    // ???
+    public boolean isFinished(); // Return if it is solved or not (litteraly getSolved)
+
+}
