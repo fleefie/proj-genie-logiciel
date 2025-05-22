47229f6824ccc6f9caef44a2d436abc0fd246b16
Happy0503 <happy0503.pro@gmail.com>
2025-05-22 19:33:03 +0200

    Added the menu for the maze generation's animation + visual improvments.

--- src/main/java/fr/cytech/projetgenielogiciel/ui/Menus.java
+++ src/main/java/fr/cytech/projetgenielogiciel/ui/Menus.java
@@ -9,11 +9,15 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Menus extends Application {
 
@@ -196,12 +200,9 @@ public class Menus extends Application {
         Button back = new Button("Back");
         Button loadState = new Button("Load state");
         Button begin = new Button("Begin");
-        back.setPrefWidth(100);
-        loadState.setPrefWidth(100);
-        begin.setPrefWidth(100);
-        back.setPrefHeight(50);
-        loadState.setPrefHeight(50);
-        begin.setPrefHeight(50);
+        back.setPrefSize(100,50);
+        loadState.setPrefSize(100,50);
+        begin.setPrefSize(100,50);
         hbox2.getChildren().addAll(back,loadState,begin);
         root.setBottom(hbox2);
 
@@ -209,12 +210,101 @@ public class Menus extends Application {
             start(primaryStage);
         });
         loadState.setOnAction(e -> {
-            // !!!!!!!!
+            // Faire popup pour récupérer un fichier
         });
         begin.setOnAction(e -> {
-            // !!!!!!!
+            mazeGeneration(primaryStage);
+            //mazeGeneration(primaryStage,posX.getText(),y,seed,algorithmChoice.getPromptText());
+        });
+
+    }
+
+    private int stepNbr = 0;
+    public void mazeGeneration(Stage primaryStage/*, int x, int y, int seed, String algorithm*/){
+        //Panel initialization
+        BorderPane root = new BorderPane();
+        root.setStyle("-fx-padding: 20;");
+        Scene scene = new Scene(root, 600, 600);
+        primaryStage.setTitle("Maze generation");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        //Top of scene
+        stepNbr = 0;
+        Image img = new Image("https://icons.veryicon.com/png/o/miscellaneous/winsion/play-button-6.png");
+        ImageView playButtonImg = new ImageView(img);
+        playButtonImg.setFitHeight(20);
+        playButtonImg.setFitWidth(20);
+        playButtonImg.setPreserveRatio(true);
+        //Text with informations
+        HBox hbox1 = new HBox();
+        hbox1.setAlignment(Pos.CENTER);
+        Label text1 = new Label("Infos du programme");
+        text1.setWrapText(true);
+        text1.setTextAlignment(TextAlignment.CENTER);
+        hbox1.getChildren().add(text1);
+        //Top buttons
+        HBox hbox2 = new HBox();
+        hbox2.setPadding(new Insets(10));
+        hbox2.setSpacing(5);
+        hbox2.setAlignment(Pos.CENTER);
+        Button saveState = new Button("Save state");
+        Button step = new Button("Step 0");
+        Button finish = new Button("Finish");
+        Button anim = new Button();
+        anim.setGraphic(playButtonImg);
+        saveState.setPrefSize(100,28);
+        step.setPrefSize(100,28);
+        finish.setPrefSize(100,28);
+        anim.setPrefSize(28,28);
+        hbox2.getChildren().addAll(saveState,step,finish,anim);
+        //Vbox with top buttons and text
+        VBox vbox1 = new VBox();
+        vbox1.setSpacing(10);
+        vbox1.setAlignment(Pos.CENTER);
+        vbox1.getChildren().addAll(hbox1,hbox2);
+        root.setTop(vbox1);
+
+        //Center of scene
+        HBox vbox2 = new HBox();
+        vbox2.setAlignment(Pos.CENTER);
+        Maze maze = new Maze(10, 10);
+        MazeView mazeView = new MazeView(maze);
+        vbox2.getChildren().addAll(mazeView);
+        root.setCenter(vbox2);
+
+        //Bottom of scene
+        HBox hbox3 = new HBox();
+        hbox3.setPadding(new Insets(10));
+        hbox3.setSpacing(30);
+        hbox3.setAlignment(Pos.CENTER);
+        Button back = new Button("Back");
+        Button endGen = new Button("End generation");
+        back.setPrefSize(100,50);
+        endGen.setPrefSize(100,50);
+        hbox3.getChildren().addAll(back,endGen);
+        root.setBottom(hbox3);
+
+
+        //Faire les boutons : saveState, step, finish
+        //Save state : save the current state of the maze in a file
+        //Step : perform a single step of the algorithm
+        //Finish : finish the generation of the maze
+        step.setOnAction(e -> {
+            stepNbr++;
+            step.setText("Step " + stepNbr);
+            //Ajouter maze.step()
+        });
+
+
+        back.setOnAction(e -> {
+            Generator(primaryStage);
+        });
+        endGen.setOnAction(e -> {
+            // Mettre dans la résolution du labyrinthe
         });
 
 
     }
+
 }
