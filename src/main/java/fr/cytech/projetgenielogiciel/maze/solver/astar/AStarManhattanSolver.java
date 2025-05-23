73e75701f7e111f25cdaf61003c0019fcba17ce7
fleefie <anon@anon.fr>
2025-05-23 04:43:26 +0200

    Changed parameters name

--- src/main/java/fr/cytech/projetgenielogiciel/maze/solver/astar/AStarManhattanSolver.java
+++ src/main/java/fr/cytech/projetgenielogiciel/maze/solver/astar/AStarManhattanSolver.java
@@ -8,8 +8,8 @@ import fr.cytech.projetgenielogiciel.maze.Maze.Position;
  * A* implementation using the Manhattan distance as a heuristic.
  */
 public class AStarManhattanSolver extends AStarSolver {
-    public AStarManhattanSolver(Maze lab, Cell start, Cell end, double pondG, double pondH) {
-        super(lab, start, end, pondG, pondH);
+    public AStarManhattanSolver(Maze lab, Cell start, Cell end, double distanceFactor, double heuristicFactor) {
+        super(lab, start, end, distanceFactor, heuristicFactor);
     }
 
     @Override
