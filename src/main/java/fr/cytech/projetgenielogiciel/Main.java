efec5b81c6571d4cb64f06d0bd628500db723d21
fleefie <anon@anon.fr>
2025-05-23 04:43:40 +0200

    UI refactor

--- src/main/java/fr/cytech/projetgenielogiciel/Main.java
+++ src/main/java/fr/cytech/projetgenielogiciel/Main.java
@@ -1,25 +1,6 @@
 package fr.cytech.projetgenielogiciel;
 
-import fr.cytech.projetgenielogiciel.maze.Maze;
-import fr.cytech.projetgenielogiciel.maze.builder.BfsBuilder;
-import fr.cytech.projetgenielogiciel.maze.builder.DfsBuilder;
-import fr.cytech.projetgenielogiciel.maze.builder.IBuilder;
-import fr.cytech.projetgenielogiciel.maze.solver.ISolver;
-import fr.cytech.projetgenielogiciel.maze.solver.TremauxSolver;
-import fr.cytech.projetgenielogiciel.maze.solver.astar.AStarDjikstraSolver;
-import fr.cytech.projetgenielogiciel.maze.solver.astar.AStarEuclideanSolver;
-import fr.cytech.projetgenielogiciel.maze.solver.astar.AStarManhattanSolver;
-import fr.cytech.projetgenielogiciel.ui.MazeView;
-import fr.cytech.projetgenielogiciel.ui.Menus;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
-import javafx.util.Duration;
+import fr.cytech.projetgenielogiciel.ui.ApplicationMain;
 import lombok.NoArgsConstructor;
 
 /**
@@ -28,14 +9,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Main {
 
-    static Menus app = new Menus();
-
     /**
      * Main method to run the project.
      *
      * @param args command line arguments
      */
     public static void main(String[] args) {
-        app.main(args);
+        ApplicationMain.launch(args);
     }
 }
---
+++ src/main/java/fr/cytech/projetgenielogiciel/ui/ApplicationMain.java
@@ -0,0 +1,62 @@
+package fr.cytech.projetgenielogiciel.ui;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+/**
+ * Main class for the application, also representing the main menu.
+ */
+public class ApplicationMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Set the scene...
+        BorderPane root = new BorderPane();
+        root.setStyle("-fx-padding: 20;");
+        Scene scene = new Scene(root, 600, 600);
+        primaryStage.setTitle("CYnapse - Main menu");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        Label title = new Label("CYnapse");
+        title.setStyle("-fx-font-size: 30px;");
+        BorderPane.setAlignment(title, Pos.TOP_CENTER);
+        root.setTop(title);
+
+        // Main menu buttons go here.
+        HBox centerButtons = new HBox(10);
+        centerButtons.setPadding(new Insets(10));
+        centerButtons.setAlignment(Pos.CENTER);
+        Button generator = new Button("Generate");
+        Button loadMaze = new Button("Load Maze");
+        Button loadBuilder = new Button("Load Builder state");
+        Button loadSolver = new Button("Load Solver state");
+
+        centerButtons.getChildren().addAll(generator, loadMaze, loadBuilder, loadSolver);
+        root.setCenter(centerButtons);
+
+        Button about = new Button("About");
+        BorderPane.setAlignment(about, Pos.BOTTOM_CENTER);
+        root.setBottom(about);
+
+        // Main menu button acctions go here.
+        generator.setOnAction(e -> new CreateGeneratorScene(primaryStage));
+        loadMaze.setOnAction(e -> {
+            /* TODO */
+        });
+        loadBuilder.setOnAction(e -> {
+            /* TODO */
+        });
+        loadSolver.setOnAction(e -> {
+            /* TODO */
+        });
+        about.setOnAction(e -> new AboutScene(primaryStage));
+    }
+}
